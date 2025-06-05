4d4367dedccfff01b073dc19d2531b86c43e5e81
eliyanur <eliyanurarrahmadan>
2025-06-05 19:33:17 +0700

    udah ni

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+ content: ["./index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3a3a3c", 
+      },
+    },
+  },
+  plugins: [],
+}
